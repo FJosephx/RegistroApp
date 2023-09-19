98beaa8cace9b1acefa6521dd2718e3a3dac2132
DamianPrograme <dami.ramos@duocuc.cl>
2023-09-19 03:11:23 -0300

    agrege en pages contrasenia correcta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,6 +36,10 @@ const routes: Routes = [
     path: 'miclase',
     loadChildren: () => import('./pages/miclase/miclase.module').then( m => m.MiclasePageModule)
   },
+  {
+    path: 'contrasenia-correcta',
+    loadChildren: () => import('./pages/contrasenia-correcta/contrasenia-correcta.module').then( m => m.ContraseniaCorrectaPageModule)
+  },
 
 ];
 
